3bd4da7e828c647f123fcb2578e37fdfc194a4ba
GHOSTBEAR <andreas.everos@gmail.com>
2019-12-04 18:07:34 +0100

    Move Listener to Application

--- src/main/kotlin/com/github/ghostbear/kotlinanilistbot/Application.kt
+++ src/main/kotlin/com/github/ghostbear/kotlinanilistbot/Application.kt
@@ -1,14 +1,38 @@
 package com.github.ghostbear.kotlinanilistbot
 
+import com.github.ghostbear.kotlinanilistbot.commands.*
+import com.github.ghostbear.kotlinanilistbot.interfaces.ICommand
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class Application
+class Application: ListenerAdapter() {
+
+	private var commands = arrayListOf<ICommand>(MangaCommand(), AnimeCommand(), CharacterCommand(), StaffCommand(), StudioCommand())
+
+	override fun onMessageReceived(event: MessageReceivedEvent) {
+		val author = event.author
+
+		if (author.isBot) {
+			return
+		}
+
+		val message = event.message
+
+		for (command in commands) {
+			if (command.matches(message)) {
+				command.execute(message)
+				break
+			}
+		}
+	}
+}
 
 fun main() {
 	val key = System.getenv("DISCORD_KEY")
 
 	val jda = JDABuilder(key)
-			.addEventListeners(SearchListener())
+			.addEventListeners(Application())
 			.build()
 
 	ActivityController(jda)
