ceb4f9f770f759fd8ad960f4bdf1614d424d323c
Andreas E <andreas.everos@gmail.com>
2019-12-05 16:25:20 +0100

    Add User-Agent to http request

--- src/main/kotlin/com/github/ghostbear/kotlinanilistbot/AniList.kt
+++ src/main/kotlin/com/github/ghostbear/kotlinanilistbot/AniList.kt
@@ -2,5 +2,5 @@ package com.github.ghostbear.kotlinanilistbot
 
 object AniList {
     const val url = "https://graphql.anilist.co/"
-    val headers = hashMapOf<String, Any>("content-type" to "application/json", "Accept" to "application/json")
+    val headers = hashMapOf<String, Any>("content-type" to "application/json", "Accept" to "application/json", "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
 }
